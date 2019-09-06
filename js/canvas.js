166054c01d2fe02e1c3d4f65697328bf6451cef2
Beyond-the-byte <info@beyondthebyte.com>
2019-09-06 21:10:40 +0530

    Basic styles and dependencies

---
+++ js/canvas.js
@@ -0,0 +1,10 @@
+// var newx = 0, newy = 0;
+// var originx = 0, originy = 0;
+// var spd = 0.09; // (val - 0.0-1.0) increase or decrease the follow through 
+
+function setup() {            
+    noStroke();            
+    smooth(8);            
+    let canvas = createCanvas(windowWidth, windowHeight);
+    canvas.parent('hero');
+}
